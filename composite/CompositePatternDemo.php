e82a2a7000b042fcb6058f2f78fa2735bb9cdfd9
KarenMrzhang <18813757480@163.com>
2019-09-26 17:09:46 +0800

    composite pattern

---
+++ composite/CompositePatternDemo.php
@@ -0,0 +1,38 @@
+<?php
+require_once "./Employee.php";
+/**
+ * Author: Karen
+ * Date: 2019/9/26 0026
+ */
+$ceo = new Employee("John","CEO",30000);
+
+$headSales = new Employee("Robert","Head Sales",20000);
+
+$headMarketing = new Employee("Michel","Head Marketing",20000);
+
+$clerk1 = new Employee("Laura","Marketing",10000);
+$clerk2 = new Employee("Bob","Marketing",10000);
+
+$salesExecutive1  = new Employee("Richard","Sales",10000);
+$salesExecutive2  = new Employee("Rob","Sales",10000);
+
+$ceo->add($headSales);
+$ceo->add($headMarketing);
+
+$headSales->add($salesExecutive1);
+$headSales->add($salesExecutive2);
+
+$headMarketing->add($clerk1);
+$headMarketing->add($clerk2);
+
+//打印该组织的所有员工
+echo $ceo . "\n";
+printAllEmployee($ceo);
+
+function printAllEmployee($employee){
+    $employees =  $employee->getSubordinates();
+    foreach ($employees as $employee){
+        echo $employee . "\n";
+        printAllEmployee($employee);
+    }
+}
---
+++ composite/Employee.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * Author: Karen
+ * Date: 2019/9/26 0026
+ */
+class Employee
+{
+    private $name;
+    private $dept;
+    private $salary;
+    private $subordinates = [];
+
+    /**
+     * Employee constructor.
+     */
+    public function __construct ($name, $dept, $sal)
+    {
+        $this->name = $name;
+        $this->dept = $dept;
+        $this->salary = $sal;
+    }
+
+    public function add($employee){
+        $this->subordinates[] = $employee;
+    }
+
+    public function remove($employee){
+        $is_exists = array_search($employee,$this->subordinates);
+        if ($is_exists === false){
+            echo 'Employee name : ' . $employee->name . 'not found !';
+        }else{
+            unset($this->subordinates[$is_exists]);
+        }
+    }
+
+    public function getSubordinates(){
+        return $this->subordinates;
+    }
+
+    public function __toString ()
+    {
+        // TODO: Implement __toString() method.
+        return "Employee :[ Name : " . $this->name
+       . ", dept : " . $this->dept . ", salary : "
+       . $this->salary ." ]";
+    }
+}
