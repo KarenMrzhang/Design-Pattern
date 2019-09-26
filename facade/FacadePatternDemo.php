eee97aa5560996207625c42273b75859ae4c1f27
KarenMrzhang <18813757480@163.com>
2019-09-26 14:48:26 +0800

    facade pattern

---
+++ facade/FacadePatternDemo.php
@@ -0,0 +1,11 @@
+<?php
+require_once "./ShapeMaker.php";
+/**
+ * Author: Karen
+ * Date: 2019/9/26 0026
+ */
+
+$shapeMaker = new ShapeMaker();
+$shapeMaker->drawCircle();
+$shapeMaker->drawRectangle();
+$shapeMaker->drawSquare();
---
+++ facade/Shape.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Author: Karen
+ * Date: 2019/9/26
+ */
+interface Shape{
+    public function draw();
+}
+
+/**
+ * Class Circle
+ * 创建实现接口的实体类。
+ */
+class Circle implements Shape{
+
+    public function draw()
+    {
+        // TODO: Implement draw() method.
+        echo "Circle:: Draw()" . "\n";
+    }
+}
+
+/**
+ * Class Rectangle
+ * 创建实现接口的实体类。
+ */
+class Rectangle implements Shape{
+
+    public function draw ()
+    {
+        // TODO: Implement draw() method.
+        echo "Rectangle:: Draw()" . "\n";
+    }
+}
+
+/**
+ * Class Square
+ * 创建实现接口的实体类。
+ */
+class Square implements Shape{
+
+    public function draw ()
+    {
+        // TODO: Implement draw() method.
+        echo "Square:: Draw()" . "\n";
+    }
+}
